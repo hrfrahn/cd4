4b782c6db2327bbe253a839645ea5c9c8c9ed10b
Harrison Frahn <harrisonfrahn@gmail.com>
2023-05-30 23:20:59 -0700

    final updates (please don't break)

--- script.js
+++ script.js
@@ -10,16 +10,30 @@ function bindBoundaryPopups(feature, layer) {
         layer.bindPopup(feature.properties.name);
     }
 }
+wCount = 0
+aCount = 0
+bCount = 0
+nCount = 0
+oCount = 0
+hCount = 0
+
+mCount = 0 
+fCount = 0
 function bindPointPopups(feature, layer) {
     if (feature.properties && feature.properties["MO Codes"]) {
         timeStr = feature.properties["Time Occurred"].toString()
-        popupText = "At "+timeStr.slice(0,timeStr.length-2)+":"+timeStr.slice(timeStr.length-2,timeStr.length)+" on "+feature.properties["Date Occurred"].slice(0,10)+", a "+feature.properties["Victim Age"]+" year old "
+        popupText = "At "+timeStr.slice(0,timeStr.length-2)+":"+timeStr.slice(timeStr.length-2,timeStr.length)+" on "+feature.properties["Date Occurred"].slice(0,10)+", a "
+        if(feature.properties["Victim Age"]){
+            popupText += feature.properties["Victim Age"]+" year old "
+        }
         gender = ""
         if(feature.properties["Victim Sex"]=="M"){
             gender = "man "
+            mCount += 1
         }
         else if(feature.properties["Victim Sex"]=="F"){
             gender = "woman "
+            fCount += 1
         }
         else{
             gender = "person "
@@ -49,6 +63,38 @@ function bindPointPopups(feature, layer) {
         popupText += ksiText
         //layer.bindPopup("<b>Date: "+feature.properties["Date Occurred"]+"<br>Age: "+feature.properties["Victim Age"]+"<br>Fatal Crash: "+feature.properties["Fatal Crash"]);
         layer.bindPopup(popupText)
+        switch(feature.properties["vict_descent"]){
+            case "W":
+                wCount += 1
+                break;
+            case "H":
+                hCount += 1
+                break;
+            case "A":
+            case "C":
+            case "D":
+            case "F":
+            case "G":
+            case "J":
+            case "K":
+            case "L":
+            case "P":
+            case "S":
+            case "U":
+            case "V":
+            case "Z":
+                aCount += 1
+                break;
+            case "B":
+                bCount += 1
+                break;
+            case "I":
+                nCount += 1
+                break;
+            default:
+                oCount += 1
+        }
+
     }
 }
 
@@ -91,10 +137,94 @@ function blackMarker(shape){
     fillOpacity: 1}
 }
 
+function getMarker(shape, color){
+    return {
+        shape: shape,
+        color: "white",
+        fillColor: color,
+        radius: 6,
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 1}
+}
 
 
 ////MO codes: 3003 = ped, 3008 = bike
 
+
+url = "https://hfrahn.pythonanywhere.com/collisions/"
+var fatal = 0
+var pedCrash = 0
+var bikeCrash = 0
+var ksiCrash = 0
+var totalcrash = 0
+var ageSum = 0
+
+var collisionsArray = []
+
+async function fetchData(years){
+    try {
+        for(i = 0; i < years.length; i++){
+            filePath = "https://hfrahn.pythonanywhere.com/collisions/"+years[i]
+            collisionsArray[i] = new L.GeoJSON.AJAX(filePath, {
+                onEachFeature: bindPointPopups,
+                pointToLayer: function(feature, latlng){
+                    totalcrash+=1
+                    if(feature.properties["Victim Age"]){
+                        ageSum += parseInt(feature.properties["Victim Age"])
+                    }
+                    //seriously injured crash
+                    if(feature.properties["MO Codes"].includes("3024")){
+                        ksiCrash += 1
+                        if(feature.properties["MO Codes"].includes("3003")){ //ped
+                            pedCrash += 1
+                            var marker = L.shapeMarker(latlng, getMarker("triangle", "orange"))
+                            return marker
+                        }
+                        bikeCrash+=1
+                         var marker = L.circleMarker(latlng, getMarker("circle", "orange"))
+                        return marker
+                    }
+                    else if(feature.properties["MO Codes"].includes("3027")){
+                        fatal += 1
+                        if (feature.properties["MO Codes"].includes("3003")){ //ped
+                            pedCrash += 1
+                            var marker = L.shapeMarker(latlng, redMarker("triangle"))
+                            // marker.addTo(allcollisions2019)
+                            // marker.addTo(fatalcollisions2019)
+                            return marker
+                        }
+                        //not ped so bike
+                        bikeCrash+=1
+                        var marker = L.circleMarker(latlng, redMarker("circle"))
+                        // marker.addTo(allcollisions2019)
+                        // marker.addTo(fatalcollisions2019)
+                        return marker
+                    }
+                    else{
+                        if (feature.properties["MO Codes"].includes("3003")){ //ped
+                            pedCrash += 1
+                            var marker =  L.shapeMarker(latlng, blackMarker("triangle"))
+                            //marker.addTo(allcollisions2019)
+                            return marker
+                        }
+                        //not ped so bike
+                        bikeCrash += 1
+                        var marker =  L.circleMarker(latlng, blackMarker("circle"))
+                       // marker.addTo(allcollisions2019)
+                        return marker
+                        
+                    }
+                    
+                }
+            })
+        }
+        return totalcrash
+    } 
+    catch (err) {
+        console.error(err);
+    }
+}
 
 
 
@@ -102,76 +232,20 @@ function blackMarker(shape){
 
 xhr = new XMLHttpRequest()
 
+var years
+
 xhr.open("GET", "https://hfrahn.pythonanywhere.com/collision_years", true)
 
 xhr.onload = () => {
     years = JSON.parse(xhr.response)
     console.log(years)
 
-    var fatal = 0
-    var pedCrash = 0
-    var bikeCrash = 0
-    var ksiCrash = 0
-
-    var collisionsArray = []
-
     for(i = 0; i < years.length; i++){
         collisionsArray.push(L.marker([0,0]))
     }
-
-    for(i = 0; i < years.length; i++){
-
-        filePath = "https://hfrahn.pythonanywhere.com/collisions/"+years[i]
-        collisionsArray[i] = new L.GeoJSON.AJAX(filePath, {
-            onEachFeature: bindPointPopups,
-            pointToLayer: function(feature, latlng){
-                // console.log(fatal)
-                // console.log(bikeCrash)
-                // console.log(pedCrash)
-                // console.log(ksiCrash)
-                //console.log(feature.properties["Date Reported"])
-                //console.log(feature.properties["Victim Age"])
-                //fatal crash
-                if(feature.properties["MO Codes"].includes("3024")){
-                    ksiCrash += 1
-                }
-                if(feature.properties["MO Codes"].includes("3027")){
-                    fatal += 1
-                    if (feature.properties["MO Codes"].includes("3003")){ //ped
-                        pedCrash += 1
-                        var marker = L.shapeMarker(latlng, redMarker("triangle"))
-                        // marker.addTo(allcollisions2019)
-                        // marker.addTo(fatalcollisions2019)
-                        return marker
-                    }
-                    //not ped so bike
-                    bikeCrash+=1
-                    var marker = L.circleMarker(latlng, redMarker("circle"))
-                    // marker.addTo(allcollisions2019)
-                    // marker.addTo(fatalcollisions2019)
-                    return marker
-                }
-                else{
-                    if (feature.properties["MO Codes"].includes("3003")){ //ped
-                        pedCrash += 1
-                        var marker =  L.shapeMarker(latlng, blackMarker("triangle"))
-                        //marker.addTo(allcollisions2019)
-                        return marker
-                    }
-                    //not ped so bike
-                    bikeCrash += 1
-                    var marker =  L.circleMarker(latlng, blackMarker("circle"))
-                   // marker.addTo(allcollisions2019)
-                    return marker
-                    
-                }
-                
-            }
-            
-        })
-        //collisionsArray[i].addTo(map)
-
-    }
+    fetchData(years)
+        
+    
     var markerClusters = L.markerClusterGroup({ maxClusterRadius: 30}).addTo(map);
 
     var dummyLayers = []
@@ -179,15 +253,10 @@ xhr.onload = () => {
         dummyLayers.push(L.marker([0,0]))
     }
 
-    console.log(dummyLayers)
-
-
-
     var overlayMaps = {}
     for(i = 0; i < years.length; i++){
         overlayMaps[years[i]] = dummyLayers[i]
     }
-    console.log(overlayMaps)
 
     var layerControl = L.control.layers(null, overlayMaps, {collapsed : false, position: 'topright'})
 
@@ -233,6 +302,7 @@ xhr.onload = () => {
         });
     });
         
+
 };
 
 xhr.send(null);
@@ -245,7 +315,7 @@ var legend = L.control({position: 'bottomleft'});
 legend.onAdd = function (map) {
  
      var div = L.DomUtil.create("div", "legend")
-     div.innerHTML += "<b>Legend</b>" + "<br>Green = Less Crashes, Red = More Crashes<br>Triangle = Pedestrian Hit by Car <br>Circle = Biker Hit by Car<br> Red Triangle/Circle = Fatal Crash"
+     div.innerHTML += "<b>Legend</b>" + "<br>Green = Less Crashes, Red = More Crashes<br>Triangle = Pedestrian Hit by Car <br>Circle = Biker Hit by Car<br> Orange Triangle/Circle = Serious Injury<br>Red Triangle/Circle = Fatal Crash"
      return div
 };
  
@@ -321,3 +391,217 @@ timeXhr.onload = () => {
 }
 
 timeXhr.send(null)
+
+
+// intersectionXhr = new XMLHttpRequest()
+
+// intersectionXhr.open("GET", "http://127.0.0.1:5000/top_intersections", true)
+
+// intersectionXhr.onload = () => {
+//     intersections = JSON.parse(intersectionXhr.response)
+//     interHTML = document.getElementById("intersection")
+//     len = Object.keys(intersections)
+//     for(i in len){
+//         interHTML.innerHTML += intersections[len[i]].inter + ": (" + intersections[len[i]].count +" crashes)<br>"
+//     }
+// }
+
+// intersectionXhr.send(null)
+
+
+
+//set up stats & charts
+//this is done a very stupid way but i really can't figure out js asnyc things lol
+//1000 ms = 1 second (should be more than enough time for data to load but can always increase)
+setTimeout(() => {
+    const ctx = document.getElementById("crashTypeChart")
+    otherCrashes = totalcrash - fatal - ksiCrash
+
+    Chart.defaults.font.size = 16
+
+    Chart.defaults.font.family = "Anuphan"
+    Chart.defaults.plugins.legend.position = "bottom"
+
+    console.log(Object.keys(collisionsArray[0]._layers).length)
+    console.log(collisionsArray[0])
+
+    yearCounts = []
+
+    for(i = 0; i < years.length; i++){
+        year = {
+            number: years[i],
+            count: Object.keys(collisionsArray[i]._layers).length
+        }
+        yearCounts.push(year)
+    }
+
+    yearCounts.sort((a, b) => a.number - b.number)
+
+    yearCounts[yearCounts.length-1].number += " (YTD)"
+
+    new Chart(ctx, {
+        type: 'doughnut', 
+        data: {
+            labels: [
+              'Victim Killed',
+              'Victim Seriously Injured',
+              'Victim Not Seriously Injured'
+            ],
+            datasets: [{
+              label: ' Crashes',
+              data: [fatal, ksiCrash, otherCrashes],
+              backgroundColor: [
+                'rgb(255, 100, 86)',
+                'rgb(255, 241, 138)',
+                'rgb(140, 186, 128)'
+              ],
+              hoverOffset: 4
+            }]
+        }, 
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Result Of Crash'
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+})
+
+    const modectx = document.getElementById("crashModeChart")
+    new Chart(modectx, {
+        type: 'doughnut', 
+        data: {
+            labels: [
+              'Biker',
+              'Pedestrian'
+            ],
+            datasets: [{
+              label: ' Crashes',
+              data: [bikeCrash, pedCrash],
+              hoverOffset: 4
+            }], 
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Type Of Crash Victim'
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+
+    const lineCtx = document.getElementById("yearLineChart")
+    new Chart(lineCtx, {
+        type: 'line', 
+        data: {
+            labels: yearCounts.map(a => a.number),
+            datasets: [{
+                label: " Crashes",
+                data: yearCounts.map(a => a.count),
+            }]
+        }, 
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Annual Crashes'
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    avgAge = ageSum / totalcrash
+
+    ageText = document.getElementById("avgAge")
+    ageText.innerHTML += parseInt(avgAge)
+
+    totalText = document.getElementById("totalCrashes")
+    totalText.innerHTML += totalcrash
+
+    deathText = document.getElementById("totalDeaths")
+    deathText.innerHTML += fatal
+    
+
+    const raceCtx = document.getElementById("raceBreakdownChart")
+
+
+    new Chart(raceCtx, {
+        type: 'doughnut', 
+        data: {
+            labels: ['White','Black', 'Hispanic/Latino','Asian American/Pacific Islander','Native American','Other/Unknown'],
+            datasets: [{
+                label: " Crashes", 
+                data: [wCount, bCount, hCount,aCount, nCount, oCount]
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Race Of Crash Victim'
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    const genderCtx = document.getElementById("genderBreakdownChart")
+
+    new Chart(genderCtx, {
+        type: 'doughnut', 
+        data:{
+            labels: ["Male", "Female"], 
+            datasets: [{
+                label: " Crashes", 
+                data: [mCount, fCount]
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Gender Of Crash Victim'
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    })
+    
+  }, "1000");
+  
